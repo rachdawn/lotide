ba22a35f31a1108f51f1af71d82b7cddf49e753a
Rachel Little <racheldawnlittle@gmail.com>
2023-10-18 00:04:17 -0600

    assertEqual compares if two values are the same

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: ", actual, " === ", expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed: ", actual, " !== ", expected);
+  }
+};
+
+// test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
