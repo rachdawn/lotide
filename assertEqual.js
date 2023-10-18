42688c0686b5dde4f170bf7da835554cbfd74637
Rachel Little <racheldawnlittle@gmail.com>
2023-10-18 00:15:58 -0600

    switched string variables to template literals added more tests

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,15 @@
 // function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: ", actual, " === ", expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌❌❌ Assertion Failed: ", actual, " !== ", expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // test code
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("cats", "kittens");
+assertEqual(1 + 1, 2);
+assertEqual(20 * 5, 90 + 10);
