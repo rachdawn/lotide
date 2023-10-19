f61a08b69a60dfa1ed6d12e676cebccea2706262
Rachel Little <racheldawnlittle@gmail.com>
2023-10-19 13:37:56 -0600

    new files

---
+++ middle.js
@@ -0,0 +1,13 @@
+// assertion test:
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays:
+
+//
+
