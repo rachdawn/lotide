9776173efd9b3c0cd36ae0e9a49e2b58ebe0bad3
Rachel Little <racheldawnlittle@gmail.com>
2023-10-22 16:33:16 -0600

    incomplete without function

---
+++ without.js
@@ -0,0 +1,36 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  const areEqual = eqArrays(arr1, arr2);
+  if (areEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// implement a without function which returns a subset of an array, removing unwanted elements
+// take in 'source' array and 'itemsToRemove' array
+// return new array, don't modify original
+
+const without = function (arr1, arr2) {
+  const source = arr1;
+  const itemsToRemove = arr2;
+  let arrayWithout = [];
+    for (let i = 0; i < arr1.length; i++) {
+    if (source.includes(arr2[i])) {
+        const arrayWithout = source.with(itemsToRemove[i]);
+    }
+  }
+  return arrayWithout;
+};
+
+// test cases:
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
