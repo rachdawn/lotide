81e5ade68af4aeb2852d80c9e99552a1320484b8
Rachel Little <racheldawnlittle@gmail.com>
2023-10-25 00:08:21 -0600

    complete assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -16,6 +16,7 @@ const assertObjectsEqual = function(actual, expected) {
     const inspect = require('util').inspect;
     if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    } else {
     console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
     }  
   };
