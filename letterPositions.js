f0d32c2d5199d9726fb9d85034a6fad0be1b0f84
Rachel Little <racheldawnlittle@gmail.com>
2023-10-24 23:36:01 -0600

    lotide letterPositions complete

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+// assertion tests:
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const areEqual = eqArrays(arr1, arr2);
+  if (areEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+//implement letterPositions function to return all indices of each letter that exists in a string
+// skip spaces
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = [sentence[i]];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+// test cases:
+assertArraysEqual(letterPositions("hello").e, [1]);
