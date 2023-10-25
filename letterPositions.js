10a2d750ad1a6cf9fb10071d8749373005e18292
Rachel Little <racheldawnlittle@gmail.com>
2023-10-24 23:38:27 -0600

    lotide letterPositions complete

--- letterPositions.js
+++ letterPositions.js
@@ -23,7 +23,7 @@ const letterPositions = function(sentence) {
   const results = {};
 
   for (let i = 0; i < sentence.length; i++) {
-    const letter = [sentence[i]];
+    const letter = sentence[i];
     if (letter !== " ") {
       if (results[letter]) {
         results[letter].push(i);
