5dd17939744aa1522def6f128250ff6d27533467
Rachel Little <racheldawnlittle@gmail.com>
2023-10-25 00:59:46 -0600

    complete findKey lotide

---
+++ findKey.js
@@ -0,0 +1,35 @@
+// assertEqual:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKey takes in an object and a callback
+// scan the object and return the first key where the callback returns truthy
+// if key not found return undefined
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+// test code:
+const result = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+assertEqual(result, "noma");
