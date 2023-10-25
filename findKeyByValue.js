22150e27167a966e51ec6754b03fbd3a5cc80036
Rachel Little <racheldawnlittle@gmail.com>
2023-10-24 23:46:18 -0600

    lotide findKeyByValue complete

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+// assert equal:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// implement function findKeyByValue
+// take in an object and a value
+// scan the object and return the first key containing the given value
+// if no key with that value return undefined
+
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// test cases:
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
