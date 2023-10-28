db58c02c2136f11eaa946d6c5c152d8926c52b37
Rachel Little <racheldawnlittle@gmail.com>
2023-10-28 13:04:52 -0600

    removed failing test cases

--- test/headTest.js
+++ test/headTest.js
@@ -5,17 +5,10 @@ describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '75'); 
-  });
   it("returns '5' for [5, 6, 7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
-  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'hello');
-  });
-
 });
