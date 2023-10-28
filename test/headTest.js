0f2d65e695b63f706242c7f76a4078a5309e91cc
Rachel Little <racheldawnlittle@gmail.com>
2023-10-28 13:02:25 -0600

    added mocha and chai, implemented in head test

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,21 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '75'); 
+  });
+  it("returns '5' for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'hello');
+  });
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["rachel"]), "rachel");
-assertEqual(head(["rachel"]), "Rachel");
-assertEqual(head([]));
+});
