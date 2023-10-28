8774f9927a25b2b7be473f7189fb74d836eeb570
Rachel Little <racheldawnlittle@gmail.com>
2023-10-28 14:15:43 -0600

    updated export object to ES6

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ const findKey = require("./findKey");
 const findKeyByValue = require("./findKeyByValue");
 const head = require("./head");
 const letterPositions = require("./letterPositions");
-const map = require("./map");
 const middle = require("./middle");
 const tail = require("./tail");
 const takeUntil = require("./takeUntil");
 const without = require("./without");
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  head: head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail: tail,
-  takeUntil: takeUntil,
-  without: without,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  middle,
+  tail,
+  takeUntil,
+  without,
 };
